73cf104a68c46ab16042f1afd0bdc96593e27f46
PhanNhaan <20520258@gm.uit.edu.vn>
2022-12-30 20:39:30 +0700

    API DATA

---
+++ APIWatchShop/APIWatchShop/Controllers/ServiceController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace APIWatchShop.Controllers
+{
+    public class ServiceController : ApiController
+    {
+        // GET api/ServiceController/HelloWebAPI
+        [Route("api/ServiceController/HelloWebAPI")]
+        [HttpGet]
+        public IHttpActionResult HelloWebAPI()
+        {
+            return Ok("Chào mừng bạn đến với Web API!");
+        }
+    }
+}
